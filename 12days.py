fc25a95145a4ae2595928af410d613fe4ddc34f7
AlexAndHisScripts <40502404+AlexAndHisScripts@users.noreply.github.com>
2020-12-20 12:13:13 +0000

    Made it 4 lines and reduced characters to 529

--- 12days.py
+++ 12days.py
@@ -1,7 +1,4 @@
-import time, winsound
-filename="12day.wav"; winsound.PlaySound(filename,winsound.SND_FILENAME); numbers=["first","second","third","fourth","fifth","sixth","seventh","eigth","ninth","tenth","eleventh","twelfth"]; gifts=["a partridge in a pear tree","turtle doves","french hens","calling birds","gold rings","geece a laying","swans a swimming","maids a milking","ladies dancing","lords a leaping","pipers piping","drummers drumming"]
-for i in range (0,12):
-    print("On the",numbers[i],"day of Christmas my true love gave to me,",i+1,gifts[i])
-    for j in range (0,i):          
-        print(i-j,gifts[i-j-1])
-    time.sleep(1);
+G=print; F=range; import winsound as B; B.PlaySound('12day.wav',B.SND_FILENAME); E=['first','second','third','fourth','fifth','sixth','seventh','eigth','ninth','tenth','eleventh','twelfth']; C=['a partridge in a pear tree','turtle doves','french hens','calling birds','gold rings','geece a laying','swans a swimming','maids a milking','ladies dancing','lords a leaping','pipers piping','drummers drumming']
+for A in F(0,12):
+	G('On the',E[A],'day of Christmas my true love gave to me,',A+1,C[A])
+	for D in F(0,A):G(A-D,C[A-D-1])
